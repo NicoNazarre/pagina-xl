cd0b06d6adc66ac177efa5af86876587860f2d6e
Nicolas Nazarre <demillamotos@yahoo.com.ar>
2024-02-14 14:44:45 -0300

    Correccion orden img carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,7 +1,7 @@
- function shiftLeft() {
+  function shiftLeft() {
     const boxes = document.querySelectorAll(".box");
     const tmpNode = boxes[0];
-    boxes[0].className = "box move-out-from-left";
+    boxes[0].className = "box move-out-from-left"; 
 
     setTimeout(function() {
         if (boxes.length > 3) {
@@ -9,8 +9,13 @@
                 boxes[i].classList.add("box--hide");
             }
         }
-        boxes[1].className = "box move-to-position1-from-left";
-        boxes[2].className = "box move-to-position2-from-left";
+        boxes[0].className = "box move-to-position1-from-left";
+        boxes[1].className = "box move-to-position2-from-left";
+        boxes[2].className = "box move-to-position3-from-left";
+        boxes[3].className = "box move-to-position4-from-left";
+        boxes[4].className = "box move-to-position5-from-left";
+        boxes[5].className = "box move-to-position6-from-left";
+        boxes[6].className = "box move-to-position7-from-left";
 
         document.querySelector(".cards__container").appendChild(tmpNode);
     }, 500);
